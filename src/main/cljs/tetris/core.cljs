798016dcd1207b4c3d65cd865a188dd695ee6beb
Mark Bastian <markbastian@gmail.com>
2017-11-08 22:05:48 -0700

    Removed html

--- src/main/cljs/tetris/core.cljs
+++ src/main/cljs/tetris/core.cljs
@@ -108,18 +108,19 @@
       (let [cell-dim 20 ;Tweak to control game size
             {:keys [locked score high-score]} @state
             sc (set (shape-coords @state))]
-           [:div
-            [:h1 "Tetris Clone"]
-            [:svg {:width (* cell-dim 10) :height (* cell-dim 22)}
-             (doall (for [i (range 10) j (range 22)]
-                         [:rect { :key (str i ":" j) :x (* i cell-dim) :y (* j cell-dim)
-                                 :width cell-dim :height cell-dim
-                                 :stroke :red :fill (cond
-                                                      (locked [i j]) :blue
-                                                      (sc [i j]) :orange
-                                                      :default :black) }]))]
-            [:h4 (str "Score: " score)]
-            [:h4 (str "High Score: " high-score)]]))
+        [:div
+         [:h1 "Tetris Clone"]
+         [:svg {:width (* cell-dim 10) :height (* cell-dim 22)}
+          (doall (for [i (range 10) j (range 22)]
+                   [:rect {:key    (str i ":" j) :x (* i cell-dim) :y (* j cell-dim)
+                           :width  cell-dim :height cell-dim
+                           :stroke :red :fill (cond
+                                                (locked [i j]) :blue
+                                                (sc [i j]) :orange
+                                                :default :black)}]))]
+         ;[:h4 (str "Score: " score)]
+         ;[:h4 (str "High Score: " high-score)]
+         ]))
 
 ;Comment out swap! form to pause the action.
 (defn sim [state] (swap! state game-step))
